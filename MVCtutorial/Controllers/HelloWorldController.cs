7f5dcf017af093367b6152820fb1a80c8bf09689
Gerhard <gerhardjpotgieter@gmail.com>
2024-06-17 17:01:18 +0200

    Adding a view

---
+++ MVCtutorial/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCtutorial.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //
+        // GET: /HelloWorld/Welcome/
+        // Requires using System.Text.Encoding.Web;
+        public IActionResult Welcome(string name, int numtimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numtimes;
+            return View();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MVCtutorial
 This repo is where I will be following the official Microsoft tutuorial
+
+# Link
+The link to the tutorial: https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/start-mvc?view=aspnetcore-8.0&tabs=visual-studio
+
